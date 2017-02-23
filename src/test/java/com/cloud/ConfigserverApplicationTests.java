9e7ecbe1dae12c197eb66e178ee12fec912d7936
Nandakumar.Menon <nandakumar82@gmail.com>
2017-02-23 20:04:57 +0530

    Committing all the files related to configserver

---
+++ src/test/java/com/cloud/ConfigserverApplicationTests.java
@@ -0,0 +1,18 @@
+package com.cloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ConfigserverApplication.class)
+@WebAppConfiguration
+public class ConfigserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
